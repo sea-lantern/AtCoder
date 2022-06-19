f603207d4036d3686e03b8c0ae1d8ae28fc7dfe2
Yamada Kaiki <west_kaiki@icloud.com>
2022-06-20 07:03:16 +0900

    arc 142

---
+++ arc/142/b.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> P;
+typedef vector<ll> VI;
+typedef vector<VI> VVI;
+#define REP(i,n) for(ll i=0;i<(n);i++)
+#define ALL(v) v.begin(),v.end()
+#define OUT(n) cout << n << "\n"
+//constexpr ll MOD=998244353;
+//constexpr ll MOD=1000000007;
+//constexpr ll INF=2e18;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    ll N;
+    cin >> N;
+
+    VI ans(N);
+
+    REP(i, N / 2 + 1){
+        ans[i * 2] = i + 1;
+        if(i * 2 + 1 < N && N % 2 == 0) ans[i * 2 + 1] = i + 1 + N / 2;
+        if(i * 2 + 1 < N && N % 2 == 1) ans[i * 2 + 1] = i + 2 + N / 2;
+    }
+
+    REP(_, N) {
+        REP(i, N){
+            cout << ans[i] << " ";
+            ans[i] += N;
+        }
+        cout << "\n";
+    }
+}
